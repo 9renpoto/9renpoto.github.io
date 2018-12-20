02afe828670a5889354c50204fe79ebb49cbf9dc
9renpoto <9renpoto+bot@gmail.com>
2018-12-20 02:49:24 +0000

    Updated

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,24 @@
 self.__precacheManifest = [
   {
-    "url": "/_next/static/~0VQuKScm1avhiGTVCOA1/pages/post.js"
-  },
-  {
-    "url": "/_next/static/~0VQuKScm1avhiGTVCOA1/pages/index.js"
-  },
-  {
-    "url": "/_next/static/~0VQuKScm1avhiGTVCOA1/pages/_error.js"
-  },
-  {
-    "url": "/_next/static/~0VQuKScm1avhiGTVCOA1/pages/_app.js"
-  },
-  {
     "url": "/_next/static/runtime/webpack-42652fa8b82c329c0559.js"
   },
   {
-    "url": "/_next/static/runtime/main-703bd22c8f25eb6fb3cd.js"
+    "url": "/_next/static/runtime/main-38a9a645117831219999.js"
   },
   {
-    "url": "/_next/static/chunks/commons.e628876160f01e276e36.js"
+    "url": "/_next/static/chunks/commons.3237ec57cac8fd0ab029.js"
+  },
+  {
+    "url": "/_next/static/UAXVHDocVGZxeLHSa08RY/pages/post.js"
+  },
+  {
+    "url": "/_next/static/UAXVHDocVGZxeLHSa08RY/pages/index.js"
+  },
+  {
+    "url": "/_next/static/UAXVHDocVGZxeLHSa08RY/pages/_error.js"
+  },
+  {
+    "url": "/_next/static/UAXVHDocVGZxeLHSa08RY/pages/_app.js"
   }
 ];
 
