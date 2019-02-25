50aa249bd3dc64500a497932d440d8c8fe0384c8
9renpoto <9renpoto+bot@gmail.com>
2019-02-25 03:53:35 +0000

    Updated

--- service-worker.js
+++ service-worker.js
@@ -6,19 +6,19 @@ self.__precacheManifest = [
     "url": "/_next/static/runtime/main-848df3dfbf4b2c6c8717.js"
   },
   {
-    "url": "/_next/static/qUrT0BQuI0XQBJVrlydkt/pages/post.js"
-  },
-  {
-    "url": "/_next/static/qUrT0BQuI0XQBJVrlydkt/pages/index.js"
-  },
-  {
-    "url": "/_next/static/qUrT0BQuI0XQBJVrlydkt/pages/_error.js"
-  },
-  {
-    "url": "/_next/static/qUrT0BQuI0XQBJVrlydkt/pages/_app.js"
-  },
-  {
     "url": "/_next/static/chunks/commons.bd8e95c1153d7fb3dc55.js"
+  },
+  {
+    "url": "/_next/static/9m3BBHu9FcRyVjREacBrm/pages/post.js"
+  },
+  {
+    "url": "/_next/static/9m3BBHu9FcRyVjREacBrm/pages/index.js"
+  },
+  {
+    "url": "/_next/static/9m3BBHu9FcRyVjREacBrm/pages/_error.js"
+  },
+  {
+    "url": "/_next/static/9m3BBHu9FcRyVjREacBrm/pages/_app.js"
   }
 ];
 
